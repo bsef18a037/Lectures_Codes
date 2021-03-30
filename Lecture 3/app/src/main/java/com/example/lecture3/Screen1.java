a3e488bd5c68acc54c19b14775abe19cfcecfbe2
Moeen Khan <63953649+bsef18a037@users.noreply.github.com>
2021-03-30 13:02:10 +0500

    Lectures Files Uploaded

---
+++ Lecture 3/app/src/main/java/com/example/lecture3/Screen1.java
@@ -0,0 +1,25 @@
+package com.example.lecture3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Screen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen1);
+    }
+    public void mainScreen(View view) {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void screen2(View view) {
+        Intent intent = new Intent(this,screen2.class);
+        startActivity(intent);
+    }
+}
---
+++ Lecture 3/app/src/main/java/com/example/lecture3/screen2.java
@@ -0,0 +1,21 @@
+package com.example.lecture3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class screen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen2);
+    }
+
+    public void mainScreen(View view) {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
